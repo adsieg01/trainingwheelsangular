00f7dcd2d6d1f69d70c13ca98b4cbb2801152fde
Joseph Bir <joe.bir.code@gmail.com>
2018-01-09 11:39:46 -0500

    Added error messages to login form

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 import { formControlBinding } from '@angular/forms/src/directives/reactive_directives/form_control_directive';
 import { AuthService } from '../../services/auth.service';
+import { validateConfig } from '@angular/router/src/config';
 
 @Component({
   selector: 'app-login',
@@ -22,8 +23,8 @@ export class LoginComponent implements OnInit {
 
   createForm() {
     this._registerForm = this._form.group({
-      email: new FormControl,
-      password: new FormControl,
+      email: new FormControl('', [Validators.email, Validators.required]),
+      password: new FormControl('', Validators.required)
     });
     console.log("createForm:", this._registerForm.value);
   }
