31f5518969c9aff00e62170bbd57d599887c2bda
Kevin Curnow <kevinscurnow@gmail.com>
2018-01-12 14:31:07 -0500

    trying to figure out activity dropdown

--- src/app/components/categorylist/categorylist.component.ts
+++ src/app/components/categorylist/categorylist.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CategoryService } from '../../services/category.service';
 import { Category } from '../../models/Category';
 import { Activity } from '../../models/Activity';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 @Component({
   selector: 'app-categorylist',
@@ -10,14 +12,24 @@ import { Activity } from '../../models/Activity';
 })
 export class CategoryListComponent {
   selectedCategory:Category = new Category(0, '--Select--');
+  selectedActivity;
   categories: Category[];
-  activities: Activity[];
+  activities: Activity;
+  
 
   constructor(private _categoryservice: CategoryService) {
     this.categories = this._categoryservice.getCategories();
    }
+  
 
-  onSelect(catid) {
-    this.activities = this._categoryservice.getActivities().filter((item)=> item.catid == catid);
+  onSelect(Category) {
+    this._categoryservice.getActivities().subscribe(activities => {
+      activities.filter((item)=> item.Category == Category)
+      console.log(activities)
+      this.selectedActivity = activities
+    });
   }
+  // onSubmit(id) {
+  //   console.log(this.activities = this._categoryservice.getActivities().filter((item)=> item.id == id));
+  // }
 }
